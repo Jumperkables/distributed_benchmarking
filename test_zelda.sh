1e20c2231f5ffece4f8f883dd590ff6773526300
Jumperkables <tom.winterbottom@msn.com>
2026-08-25 10:59:20 +0100

    fixing startup problems

--- test_zelda.sh
+++ test_zelda.sh
@@ -3,6 +3,7 @@ export TORCH_DISTRIBUTED_DEBUG=DETAIL
 export TORCH_CPP_LOG_LEVEL=INFO
 export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=ALL
+export NCCL_SOCKET_IFNAME==enp15s0
 torchrun \
     --nnodes=2 \
     --nproc-per-node=1 \
