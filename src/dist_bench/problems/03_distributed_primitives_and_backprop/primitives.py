824818489ef688e633d613e91a1de3f5fbcc23ff
Jumperkables <tom.winterbottom@msn.com>
2026-08-28 07:57:16 +0100

    setting up primitives implementations

--- src/dist_bench/problems/03_distributed_primitives_and_backprop/primitives.py
+++ src/dist_bench/problems/03_distributed_primitives_and_backprop/primitives.py
@@ -14,69 +14,105 @@ from dist_bench.common.my_utils import (
     RHEADER
 )
 
+# Global
+HOST, RANK, LOCAL_RANK, WORLD_RANK, MASTER_ADDR, MASTER_PORT = get_dist_env_info()
+DEVICE = torch.device(f"cuda:{LOCAL_RANK}")
+
 
 ####################
 # Collectives
 def broadcast():
-    print("TODO")
+    val = float(RANK + 1)
+    rank_b = 1
+    if RANK == rank_b:
+        x = torch.ones(1024, device=DEVICE)*val
+    dist.broadcast(x, src=rank_b)
+    rprint(x)
+    dist.barrier(device_ids=[LOCAL_RANK])
+
+
+def all_to_all():
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def scatter():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+
+
+def reduce_scatter():
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def all_reduce():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def reduce():
-    print("Todo")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def all_gather():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def gather():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 ####################
 # P2P
 def send():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 def recv():
-    print("TODO")
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 ####################
 # Blocking vs non-blocking
-def blocking():
-    print("TODO")
+def blocking_vs_nonblocking():
+    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+
 
 
 # Main
 def main():
     # Print dist info
     print_dist_env_info()
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
-    device = torch.device(f"cuda:{local_rank}")
+
 
     # Init process
     dist.init_process_group(
         backend="nccl",
-        device_id=device,
+        device_id=DEVICE,
     )
 
-    #
-    x = torch.tensor([float(rank + 1)], device=device)
-    dist.all_reduce(x, op=dist.ReduceOp.SUM)
-    expected = 3.0
-    if x.item() != expected:
-        raise RuntimeError(RHEADER+f"expected {expected}, got {x.item()}")
-    dist.barrier(device_ids=[local_rank])
+    ####################
+    # Collectives
+    broadcast()
+    all_to_all()
+    reduce_scatter()
+    scatter()
+    all_reduce()
+    reduce()
+    all_gather()
+    gather()
+
+
+    ####################
+    # P2P
+    send()
+    recv()
+
+
+    ####################
+    # Blocking vs non-blocking
+    blocking_vs_nonblocking()
+
+
+    ####################
+    # Cleanup
     dist.destroy_process_group()
 
 
