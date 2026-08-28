570f76c5c7cb9075365dac528f5f7802f7334a3c
Jumperkables <tom.winterbottom@msn.com>
2026-08-28 12:52:30 +0100

    primitives for torch.dist thoroughly explored

--- src/dist_bench/problems/03_distributed_primitives_and_backprop/README.md
+++ src/dist_bench/problems/03_distributed_primitives_and_backprop/README.md
@@ -35,3 +35,118 @@ identical gradients
 optimizer.step()
 
 ## Primitives
+`broadcast()`
+- Taught me I still need to instantiate variables to be received in broadcast()
+- Guessed uses in ML:
+  - I can see broadcast being a naive way to share weight tensors across models. I know we have ring reduce nowadays which does not require a gather scatter endpoint
+  - For modern uses, I wonder if optimiser states (momentum etc...) are gathered and broadcast?
+
+`scatter()`
+- Seemed rather straightforward syntactically
+- Guessed uses in ML:
+  - For when multiple nodes carry shards of an entire model's weights, I can see the initial step of loading and distributing model tensors (a list of parameter tensors) being `torch.chunk`'d and then scattered across ranks.
+  - I expected modern practices not to every bother with this from one central node. I imagine each individual node carries literally only a shard from disk which it loads itself.
+
+`gather()`
+- Pretty straightforward
+
+`all_gather() <- standard, object, and coalesced variants` 
+- SOME of these can notably handle variably sized tensors
+- `all_gather()` surprised me at first. I assumed you'd be able to pass an empty list, but you have to imitate the sizes of the incoming expected tensors. Allows variably sized tensors.
+- `all_gather_single()`
+  - This is awesome. All the incoming tensors are gathered into a single tensor object ready to go, rather than just a list. And, according to the examples it looks like depending on the size of the receiving input dummy tensor, I can have the incoming tensor be in concat OR stacked format
+  - Love the elegance of this method
+- `all_gather_coalesced()`
+  - I wondered if coalescence meant something similar to how it does in CUDA, it turns out yeah kinda
+  - The docs are warning that in [2.13](https://docs.pytorch.org/docs/2.13/distributed.html#torch.distributed.all_gather_coalesced) shape checking across all nodes isn't checked to allow for maximal performance and that we should be careful for erroneous errors
+  - Turns out for the `nccl` backend i'll be using for ML workloads, its not implemented. Which is interesting
+- `all_gather_object()`
+  - Looks like theres a variation in `collective_utils` which also does type checking on the back end and errors every rank if it fails. In the docs, not in my build.
+  - Uses pickle, and will go via the CPU, since its designed for a wide array of objects. So obviously don't call it for GPU tensors for a pointless `GPU <-> CPU` transfer.
+  - I ran into strange behaviour, where code wouldnt work without `torch.cuda.set_device()` set explicitly. Even though the objects I created seemed to have nothing to do with the GPU. Strange
+- `all_gather_into_tensor()`
+  - Deprecated. This and coalesced are clearly implement better functionally by single.
+
+`reduce()`
+- Very straightforward after experience with the other varieties of primitives
+- Reduction between tensors mutually. i.e. sum all the values between tensors mutually
+
+`all_reduce()` Also very straightforward now. Though interestingly here I experimented with the reduction ops they made, `dist.PREMUL_SUM(0.1)` for example is very handy!
+
+`reduce_scatter()`
+- Functionally understanding what its doing was quite easy. But conceptually why this kind of operation is ever useful was quite tricky. I think the clue is in the ML application mentioned:
+```
+Suppose a model has layers with these parameters:
+Layer 1: 1000 parameters
+Layer 2: 2000 parameters
+Layer 3: 3000 parameters
+
+After backwards, we have gradients corresponding to these 6000
+gradient = [g0, g1, ..., g5999]
+
+We could arbitrarily shared these gradients across 3 GPUs:
+- GPU0 owns: [g0,    ..., g1999]
+- GPU1 owns: [g2000, ..., g3999]
+- GPU2 owns: [g4000, ..., g5999]
+
+These shared don't always correspond to layers:
+GPU 0: half of Layer 1 + part of Layer 2
+GPU 1: remainder of Layer 2 + part of Layer 3
+GPU 2: remainder of Layer 3
+
+             gradient shard
+
+GPU 0: [A0 | A1 | A2]
+GPU 1: [B0 | B1 | B2]
+GPU 2: [C0 | C1 | C2]
+
+all reduce would give:
+GPU 0: [A0+B0+C0 | A1+B1+C1 | A2+B2+C2 ]
+GPU 1: [A0+B0+C0 | A1+B1+C1 | A2+B2+C2 ]
+GPU 2: [A0+B0+C0 | A1+B1+C1 | A2+B2+C2 ]
+
+          ↓ reduce-scatter
+
+GPU 0: [A0+B0+C0]
+GPU 1: [A1+B1+C1]
+GPU 2: [A2+B2+C2]
+```
+
+`all_to_all()`
+- Basically a large permutation command without reduction.
+- From the 2.13 docs as a reminder:
+```
+>>> input = torch.arange(4) + rank * 4
+>>> input = list(input.chunk(4))
+>>> input
+[tensor([0]), tensor([1]), tensor([2]), tensor([3])]     # Rank 0
+[tensor([4]), tensor([5]), tensor([6]), tensor([7])]     # Rank 1
+[tensor([8]), tensor([9]), tensor([10]), tensor([11])]   # Rank 2
+[tensor([12]), tensor([13]), tensor([14]), tensor([15])] # Rank 3
+>>> output = list(torch.empty([4], dtype=torch.int64).chunk(4))
+>>> dist.all_to_all(output, input)
+>>> output
+[tensor([0]), tensor([4]), tensor([8]), tensor([12])]    # Rank 0
+[tensor([1]), tensor([5]), tensor([9]), tensor([13])]    # Rank 1
+[tensor([2]), tensor([6]), tensor([10]), tensor([14])]   # Rank 2
+[tensor([3]), tensor([7]), tensor([11]), tensor([15])]   # Rank 3
+```
+
+## P2P
+`send()` and `recv()`
+These were quite straightforward. They need to be paired. But super importantly:
+
+**With an NCCL backend, there is no notion of tags**
+
+If you're familiar with MPI this might sound strnage. I'm not an MPI expert but its crazy enough to me. Instead we rely on operation ordering. How strange.
+
+
+
+# Minimal NN forward and backprop
+TODO:
+- Minimal neural network forward pass and backprop
+- Make sure that the gradients and optimiser states are the same on both nodes
+- Use torch.dist primitives to support it
+
+# Blocking vs Asynchronous
+A small exploration here
--- src/dist_bench/problems/03_distributed_primitives_and_backprop/primitives.py
+++ src/dist_bench/problems/03_distributed_primitives_and_backprop/primitives.py
@@ -15,8 +15,9 @@ from dist_bench.common.my_utils import (
 )
 
 # Global
-HOST, RANK, LOCAL_RANK, WORLD_RANK, MASTER_ADDR, MASTER_PORT = get_dist_env_info()
+HOST, RANK, LOCAL_RANK, WORLD_SIZE, MASTER_ADDR, MASTER_PORT = get_dist_env_info()
 DEVICE = torch.device(f"cuda:{LOCAL_RANK}")
+torch.cuda.set_device(DEVICE)
 
 
 ####################
@@ -29,14 +30,17 @@ def broadcast():
     else:
         x = torch.ones(1024, device=DEVICE)*999
     dist.broadcast(x, src=rank_b)
-    rprint(x)
     dist.barrier(device_ids=[LOCAL_RANK])
     success = torch.all(x == 1)
     rprint(f"BROADCAST {success}: Expected all values in tensor == 1")
 
 
 def all_to_all():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+    inputs = torch.arange(2, device=DEVICE) + RANK * 2
+    inputs = list(inputs.chunk(2))
+    outputs = list(torch.empty([2], device=DEVICE, dtype=torch.int64).chunk(2))
+    dist.all_to_all(outputs, inputs)
+    rprint(f"ALL_TO_ALL: Expected a permuted version of the input tensors{outputs}")
 
 
 def scatter():
@@ -48,38 +52,116 @@ def scatter():
     output_tensor = torch.zeros(10, device=DEVICE)
     dist.scatter(output_tensor, to_scatter, src=rank_s)
     success = torch.all(output_tensor == RANK+1)
-    rprint(f"Scatter {success}: Expected all values in tensor == RANK+1, here {output_tensor[0]}")
+    rprint(f"SCATTER {success}: Expected all values in tensor == RANK+1, here {output_tensor[0]}")
 
 
 
 def reduce_scatter():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+    output = torch.zeros(4, device=DEVICE, dtype=torch.float32)
+    input_list = [
+        torch.arange(4, device=DEVICE, dtype=torch.float32)    * (10**RANK),
+        torch.arange(4, 8, device=DEVICE, dtype=torch.float32) * (10**RANK),
+    ]
+    rprint(input_list)
+    dist.reduce_scatter(output, input_list)
+    rprint(f"REDUCE_SCATTER: {output}")
+
 
 
 def all_reduce():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+    tensor = torch.ones(10, device=DEVICE) * (RANK+1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.PRODUCT)
+    rprint(f"ALL_REDUCE: Expecting the same tensor on EACH node filled with values {WORLD_SIZE*(WORLD_SIZE+1)/2} {tensor}")
+
+    premul_sum_factor = 0.1
+    tensor = torch.ones(10, device=DEVICE) * (RANK+1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.PREMUL_SUM(premul_sum_factor))
+    rprint(f"ALL_REDUCE: -PREMUL_SUM check- Expecting the same tensor on EACH node filled with values { premul_sum_factor* WORLD_SIZE * (WORLD_SIZE + 1) / 2} {tensor}")
+
 
 
 def reduce():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+    rank_r = 0
+    tensor = torch.ones(10, device=DEVICE) * (RANK+1)
+    dist.reduce(tensor, dst=rank_r, op=dist.ReduceOp.SUM)
+    rprint(f"REDUCE: Expecting a tensor filled with {WORLD_SIZE*(WORLD_SIZE+1)/2} on dst node {rank_r}: {tensor}")
 
 
 def all_gather():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+    # Try out the coalesced and object variants
+    # I think all nodes get the gather output
+    rank_g = 1
+    tensor_list = [torch.zeros(1, device=DEVICE), torch.zeros(2, device=DEVICE)]
+    tensor = torch.ones(RANK+1, device=DEVICE)*(RANK+1)
+    dist.all_gather(tensor_list, tensor)
+    rprint(f"ALL_GATHER: Expect each rank to have the same gathered list: {tensor_list}")
+
+
+def all_gather_single():
+    rank_g = 0
+    size_t = 3
+    output_tensor_stacked = torch.zeros((WORLD_SIZE, size_t), device=DEVICE)
+    output_tensor_concat = torch.zeros(WORLD_SIZE * size_t, device=DEVICE)
+    input_tensor = torch.ones(size_t, device=DEVICE) * (RANK+1)
+    dist.all_gather_single(output_tensor_stacked, input_tensor)
+    dist.all_gather_single(output_tensor_concat, input_tensor)
+    rprint("ALL_GATHER_SINGLE: Stacked format", output_tensor_stacked)
+    rprint("ALL_GATHER_SINGLE: Concat format", output_tensor_concat)
+
+
+def all_gather_coalesced():
+    # Varying sized tensors are allowed here
+    output_tensor_list = [
+        [
+            torch.tensor([-1, -1], device=DEVICE),
+            torch.tensor([-1], device=DEVICE),
+            torch.tensor([-1, -1, -1], device=DEVICE)
+        ] for _ in range(WORLD_SIZE)
+    ]
+    input_tensor_list = [
+        torch.ones(2, device=DEVICE) * (RANK+1),
+        torch.ones(1, device=DEVICE) * (RANK+1),
+        torch.ones(3, device=DEVICE) * (RANK+1),
+    ]
+    dist.all_gather_coalesced(output_tensor_list, input_tensor_list)
+    rprint(f"ALL_GATHER_COALESCED: Deprecated, but should expect a coalesced form of tensors from both ranks {output_tensor_list}")
+
+
+def all_gather_object():
+    # This example i borrow from docs, mine wasnt working
+    gather_objects = [None for _ in range(WORLD_SIZE)]  # any picklable object
+    gather_objects[RANK] = f"Hello Edd boy {RANK}"
+    output = [None for _ in gather_objects]
+    dist.all_gather_object(output, gather_objects[dist.get_rank()])
+    rprint(f"ALL_GATHER_OBJECT: Should have dictionaries with rank aligned numbers in {output}")
+    #torch.distributed.collective_utils.all_gather_object_enforce_type()
 
 
 def gather():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
-
+    # An inverse to scatter in a sense
+    rank_g = 0  # destination rank for gather
+    size_t = 2
+    if RANK == rank_g:
+        gather_list = [torch.zeros(size_t, device=DEVICE) for _ in range(size_t)]
+    else:
+        gather_list = None
+    to_gather = (torch.ones(size_t, device=DEVICE) * RANK)
+    dist.gather(to_gather, gather_list, dst=rank_g)
+    rprint(f"GATHER: Expected gather rank to contain list of tensors, and everything else as None: {gather_list}")
 
 ####################
 # P2P
-def send():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
+def send_and_recv():
+    rank_src = 0
+    rank_dst = 1
+    tensor = torch.ones(10, device=DEVICE) * (RANK+1)
+    rprint(f"SEND_AND_RECV: Before send from {rank_src} to {rank_dst}: {tensor}")
+    if RANK == rank_src:
+        dist.send(tensor, rank_dst)
+    if RANK == rank_dst:
+        dist.recv(tensor, rank_src)
+    rprint(f"SEND_RECV: After send from {rank_src} to {rank_dst}: {tensor}")
 
-
-def recv():
-    host, rank, local_rank, world_size, master_addr, master_port = get_dist_env_info()
 
 
 ####################
@@ -104,24 +186,25 @@ def main():
     ####################
     # Collectives
     broadcast()
-    all_to_all()
-    reduce_scatter()
     scatter()
-    all_reduce()
-    reduce()
-    all_gather()
     gather()
+    all_gather()
+    all_gather_single()
+    #all_gather_coalesced() #<- keeping this cos theres a maybe typo in the docs i can PR =-) so i'll remember where it is
+    all_gather_object()
+    reduce()
+    all_reduce()
+    reduce_scatter()
+    all_to_all()
 
 
     ####################
     # P2P
-    send()
-    recv()
+    send_and_recv()
 
 
     ####################
     # Blocking vs non-blocking
-    blocking_vs_nonblocking()
 
 
     ####################
